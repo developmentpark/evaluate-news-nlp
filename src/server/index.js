07cbe3ed86e38849374484b801544a77c57a803c
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-13 14:32:19 -0400

    adjusted the path for serving static resources on the server

--- src/server/index.js
+++ src/server/index.js
@@ -5,14 +5,14 @@ const mockAPIResponse = require("./mockAPI.js");
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static("build"));
 app.use(cors());
 
 console.log(__dirname);
 
 app.get("/", function (req, res) {
   // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile(path.resolve("./build/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
