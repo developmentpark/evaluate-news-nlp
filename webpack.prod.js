c2ff0f8e84ae8b2b78f5bf89a8bb9d9de80d3f0e
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-13 14:32:08 -0400

    cofig. Webpack for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,18 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    libraryTarget: "var",
+    library: "Client",
+    clean: true,
+  },
   module: {
     rules: [
       {
@@ -12,12 +20,20 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
   ],
+  optimization: {
+    minimizer: ["...", new CssMinimizerWebpackPlugin()],
+  },
 };
