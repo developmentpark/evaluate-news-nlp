3f52471bec3db394b681e1c33b7c4c8f39443e2a
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-21 07:41:33 -0500

    refactor

--- src/client/utils/fragmentBySegmentDepth.js
+++ src/client/utils/fragmentBySegmentDepth.js
@@ -1,33 +1,43 @@
-export function fragmentBySegmentDepth(sentence, deep = 1) {
-  const arr = [];
-  let start = 0;
-  let end = sentence.endp - sentence.inip + 1;
-  for (let segment of sentence.segment_list || []) {
-    //fragment belonging only to the sentence
-    const fragment = {
-      text: sentence.text.slice(start, segment.inip - sentence.inip),
-      deep: deep,
-      confidence: sentence.confidence,
-      score_tag: sentence.score_tag,
-      agreement: sentence.agreement,
-    };
-    //arr.push(sentence.text.slice(start, segment.inip - sentence.inip));
-    arr.push(fragment);
-    //the segment
-    arr.push(...fragmentBySegmentDepth(segment, deep + 1));
-    start = segment.endp - sentence.inip + 1;
-  }
-  //the last fragment belonging only to the sentence
-  // or the sentence itself if it does not have segments
-  const fragment = {
-    text: sentence.text.slice(start, end),
+/**
+ * Creates a fragment object with the provided text and attributes from a sentence object.
+ *
+ * @param {string} text - The text of the fragment.
+ * @param {object} sentence - The sentence object from which attributes are obtained.
+ * @param {number} deep - Indicates the level of division to which the fragment belongs.
+ * @returns {object} - The created fragment object.
+ */
+function createFragment(text, sentence, deep) {
+  return {
+    text,
     deep: deep,
     confidence: sentence.confidence,
     score_tag: sentence.score_tag,
     agreement: sentence.agreement,
   };
-  arr.push(fragment);
-  //arr.push(sentence.text.slice(start, end));
+}
 
+/**
+ * Divides a sentence into an array of fragments by dividing the sentence into segments recursively.
+ * Sentence separators are the segments at that depth level.
+ * The objects in the segment_list attribute of the sentence object are used as segments.
+ *
+ * @param {object} sentence - The sentence object to be fragmented.
+ * @param {number} deep - Indicates the depth level of the current sentence (default is 1).
+ * @returns {Array} - An array of fragments obtained through segmentation.
+ *
+ * @see {@link https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/response} for expected sentence format.
+ */
+export function fragmentBySegmentDepth(sentence, deep = 1) {
+  const arr = [];
+  let start = 0;
+  let end = sentence.endp - sentence.inip + 1;
+  for (let segment of sentence.segment_list || []) {
+    const text = sentence.text.slice(start, segment.inip - sentence.inip);
+    arr.push(createFragment(text, sentence, deep));
+    arr.push(...fragmentBySegmentDepth(segment, deep + 1));
+    start = segment.endp - sentence.inip + 1;
+  }
+  const text = sentence.text.slice(start, end);
+  arr.push(createFragment(text, sentence, deep));
   return arr;
 }
