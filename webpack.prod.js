e21b3e557292ec9dc2b7889d2efe884f9d6d7e59
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-27 06:22:34 -0500

    refactor

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 
