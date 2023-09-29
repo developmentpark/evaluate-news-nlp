2588cd378d821d6d0dc78e7b573e2f6dab4dd7ad
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-28 23:42:47 -0500

    add list of possible messages that will be shown #11

---
+++ src/client/js/messages.js
@@ -0,0 +1,10 @@
+const messages = {
+  SUCCESS: "Analysis completed successfully!",
+  INVALID_INPUT: "Invalid format for URL.",
+  NETWORK_ERROR:
+    "Sorry, we were unable to complete your request at this time. Please try again later.",
+  SERVER_ERROR:
+    "We're sorry, something went wrong on our server. Please try again later.",
+};
+
+export { messages };
