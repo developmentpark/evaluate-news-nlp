3010dd6bb6d7055854e5798a72cdd2e497b2bcf1
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-09-29 01:43:57 -0500

    Added renderAlert function to display messages #11

--- src/client/js/render.js
+++ src/client/js/render.js
@@ -1,11 +1,21 @@
+import { getEl } from "../utils/domReader";
 import { resultsView } from "./resultView";
 
 function renderError(data) {
   console.log(data);
 }
 
+function renderAlert(message) {
+  const alert = getEl("alert");
+  alert.textContent = message.text;
+  alert.classList.add(`alert_${message.type}`, "open");
+  setTimeout(() => {
+    alert.classList.remove("open");
+  }, 4000);
+}
+
 function renderResults(data) {
   document.querySelector(".results-section").innerHTML = resultsView(data);
 }
 
-export { renderError, renderResults };
+export { renderError, renderResults, renderAlert };
